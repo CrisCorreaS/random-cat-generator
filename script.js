666cd625f4bd8adb6ebdee310b27e2abb7ef4b32
Cristina Correa <cristina.correa.segade@gmail.com>
2024-02-26 05:53:43 +0100

    update

--- script.js
+++ script.js
@@ -7,27 +7,14 @@ let gatitos = [];
 let numero = 1;
 
 async function showGatito(num) {
-  /*
-  Si lo pongo de esta forma:
-      enlace.href = await gatitos[num - 1].img;
-      imagen.src = await gatitos[num - 1].img;
-      parrafo.innerHTML = "Este es el gatito número " + num;
+  const imgElement = new Image();
+  imgElement.src = gatitos[num - 1].img;
 
-  La imagen sigue cargándose bastante después que el texto del párrafo
+  await imgElement.decode();
 
-  Por eso tenemos que hacer lo siguiente:
-  */
-  const imgElement = new Image(); // creamos una nueva instancia de un objeto "Image". Este objeto representa un elemento <img> en el DOM pero puede ser manipulado sin tener que agregarlo al DOM directamente
-  imgElement.src = gatitos[num - 1].img; // asignamos la URL de la imagen que deseamos cargar a la propiedad "src" del objeto "Image". Cuando se asigna una URL a "src", el navegador comienza a cargar la imagen desde esa URL
-
-  // Esperamos a que la imagen se cargue y decodifique
-  await imgElement.decode(); // utilizamos el método decode() del objeto "Image". Este método devuelve una promesa que se resuelve cuando la imagen ha terminado de cargarse y decodificarse
-
-  // Ahora que la imagen está lista, actualizamos el enlace y la fuente de la imagen
   enlace.href = gatitos[num - 1].img;
   imagen.src = gatitos[num - 1].img;
 
-  // Y finalmente actualizamos el contenido del párrafo
   parrafo.innerHTML = "Este es el gatito número " + num;
   numero++;
 }
@@ -38,7 +25,7 @@ function addGatito() {
     dataType: "json",
     success: function (data) {
       gatito = {
-        img: data[0].url, // Cuidado porque si en vez de (data) usas $(this) no funciona
+        img: data[0].url,
       };
 
       gatitos.push(gatito);
@@ -49,4 +36,4 @@ function addGatito() {
 }
 
 document.addEventListener("DOMContentLoaded", addGatito, false);
-document.addEventListener("click", addGatito, false);
+siguiente.addEventListener("click", addGatito, false);
